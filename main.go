9a45784623dcdab9eae0646ec4840b030e01b1d3
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2023-10-23 12:48:26 +0100

    wire up msg channel

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Server struct {
     listenAddr string 
@@ -16,17 +19,50 @@ func NewServer(listenAddr string) *Server {
 }
 
 func (s *Server) Start() error {
-    ln, err := net.Listen("tcp", "8000")
+    ln, err := net.Listen("tcp", s.listenAddr)
     if err != nil {
         return err
     }
     defer ln.Close()
     s.ln = ln
 
+    go s.acceptLoop()
+
     <-s.quitCh
     return nil
 }
 
+func (s *Server) acceptLoop() {
+    for{
+        conn, err := s.ln.Accept()
+        if err != nil {
+            fmt.Println("Accept Error:", err)
+            continue
+        }
+
+
+        fmt.Println("new connection to  the server:", conn.RemoteAddr())
+        go s.readLoop(conn)
+    }
+}
+
+
+func (s *Server) readLoop(conn net.Conn){
+    defer  conn.Close()
+    buf := make([]byte, 2048)
+    for {
+        n, err := conn.Read(buf)
+        if err !=nil {
+            fmt.Println("Read Error: ", err)
+            continue
+        }
+
+        msg := buf[:n]
+        fmt.Println(string(msg))
+    }
+} 
+
 func main() {
-    print("yppp")
+   server := NewServer(":3000")
+   server.Start()
 }
